ecb2a51ad4fee16f55e207df26f51b3f37442bb9
mornestroebel <morne@bitcube.tech>
2025-08-05 20:57:17 +0200

    feat(navigation): build a navigation system

---
+++ lib/core/navigation/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sentri/src/screens/login/login.dart';
+import 'package:sentri/src/screens/splash/splash.dart';
+
+class Routes {
+  static const String splash = '/';
+  static const String login = '/login';
+
+  static Map<String, Widget Function(Object? params)> map = {
+    splash: (Object? params) => const Splash(),
+    login: (Object? params) => const Login()
+  };
+}
