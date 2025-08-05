0c2ca41b462af865682d1e52a01524e093b75b32
mornestroebel <morne@bitcube.tech>
2025-08-05 21:12:19 +0200

    feat(navigation): add time delay on splash

--- lib/src/screens/splash/splash.dart
+++ lib/src/screens/splash/splash.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:sentri/core/navigation/routes.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      if (!mounted) return;
+      Navigator.of(context).pushReplacementNamed(Routes.login);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(child: Column(children: [Text("Splash Screen")])),
+    );
   }
 }
